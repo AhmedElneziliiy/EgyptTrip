03aa265e00829bbf568c888659ba2357396936fe
Ahmed Elnezily <elneziliiyahmed@gmail.com>
2025-07-24 03:57:02 +0300

    first commit

---
+++ .angular/cache/19.2.15/TripLink/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,17 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-UQWNYCER.js";
+import "./chunk-GBANFASH.js";
+import "./chunk-QZ2E34AJ.js";
+import "./chunk-5QXQOMPN.js";
+import "./chunk-P6U2JBMQ.js";
+import "./chunk-WDMUDEB6.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
